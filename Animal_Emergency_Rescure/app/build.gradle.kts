b66b64299158e9c9dfde1d8fa3bf82ab241e771f
pei <hunterlin2014@gmail.com>
2024-05-20 03:44:43 +0800

    #19 edit recruit UI

--- Animal_Emergency_Rescure/app/build.gradle.kts
+++ Animal_Emergency_Rescure/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     testImplementation("junit:junit:4.13.2")
