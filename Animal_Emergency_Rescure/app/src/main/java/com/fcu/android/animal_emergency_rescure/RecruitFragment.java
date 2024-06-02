8fe81f0db185dfa8588a2feef2a8d0ac674c9efa
pei <hunterlin2014@gmail.com>
2024-06-03 01:10:30 +0800

    Revert "#19 fix the return function bug"
    This reverts commit 8c777453387dc1ec806d2e04ea5ae659a068f850.

--- Animal_Emergency_Rescure/app/src/main/java/com/fcu/android/animal_emergency_rescure/RecruitFragment.java
+++ Animal_Emergency_Rescure/app/src/main/java/com/fcu/android/animal_emergency_rescure/RecruitFragment.java
@@ -3,40 +3,35 @@ package com.fcu.android.animal_emergency_rescure;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class RecruitFragment extends Fragment {
-
-    private FragmentManager manager;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_recruit, container, false);
-        // 取得 FragmentManager 物件
-        manager = getActivity().getSupportFragmentManager();
-        // 設定按鈕的監聽事件
-        view.findViewById(R.id.btn_cash).setOnClickListener(this::onClick);
-        view.findViewById(R.id.btn_member).setOnClickListener(this::onClick);
-        return view;
-    }
+        // Find the views
+        Button btnCash = view.findViewById(R.id.btn_cash);
+        Button btnMember = view.findViewById(R.id.btn_member);
+        // 跳轉至捐款招募頁面
+        btnCash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, new CashRecruitFragment()).commit();
+            }
+        });
+        // 跳轉至志願招募頁面
+        btnMember.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, new MemberRecruitFragment()).commit();
+            }
+        });
 
-    public void onClick(View v) {
-        // 開始一個 FragmentTransaction 的交易
-        FragmentTransaction transaction = manager.beginTransaction();
-        if (v.getId() == R.id.btn_cash) {
-            // 跳轉到現金招募 Fragment
-            transaction.replace(R.id.container, new CashRecruitFragment()).commit();
-        } else if (v.getId() == R.id.btn_member) {
-            // 跳轉到會員招募 Fragment
-            transaction.replace(R.id.container, new MemberRecruitFragment()).commit();
-        }
-        // 將 Fragment 加入到返回堆疊
-        transaction.addToBackStack(null);
+        return view;
     }
 }
