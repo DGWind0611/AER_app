d2755184a30c93a6e99326c66f6839989eef8b7f
pei <hunterlin2014@gmail.com>
2024-06-10 01:43:15 +0800

    #15 create forget password function

---
+++ Animal_Emergency_Rescure/app/src/main/java/com/fcu/android/animal_emergency_rescure/ForgetPasswordActivity.java
@@ -0,0 +1,61 @@
+package com.fcu.android.animal_emergency_rescure;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+
+    private EditText etEmail;
+    private Button btnSendEmail;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+
+        etEmail = findViewById(R.id.et_email_fp);
+        btnSendEmail = findViewById(R.id.btn_send_email);
+        mAuth = FirebaseAuth.getInstance();
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = etEmail.getText().toString();
+                if (email.isEmpty()) {
+                    etEmail.setError("請輸入電子郵件地址");
+                } else {
+                    mAuth.sendPasswordResetEmail(email)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        // 重設密碼信件已寄出
+                                        Toast.makeText(ForgetPasswordActivity.this, "重設密碼信件已寄出", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        // 電子郵件地址不存在
+                                        etEmail.setText("");
+                                        etEmail.setError("電子郵件地址不存在");
+                                    }
+                                }
+                            });
+                }
+            }
+        };
+        btnSendEmail.setOnClickListener(listener);
+    }
+}
