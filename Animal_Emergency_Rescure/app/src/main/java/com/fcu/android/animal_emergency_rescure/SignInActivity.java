862c2799a3e09d5a94c80417f19361df58cc2821
pei <hunterlin2014@gmail.com>
2024-06-10 01:45:37 +0800

    #15 update auth function

--- Animal_Emergency_Rescure/app/src/main/java/com/fcu/android/animal_emergency_rescure/SignInActivity.java
+++ Animal_Emergency_Rescure/app/src/main/java/com/fcu/android/animal_emergency_rescure/SignInActivity.java
@@ -2,7 +2,6 @@ package com.fcu.android.animal_emergency_rescure;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,17 +16,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import java.security.SecureRandom;
-
-import kotlin.random.URandomKt;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignInActivity extends AppCompatActivity {
 
     private EditText etEmail, etPassword;
     private TextView tvForgetPassword, tvSignUp;
-    private Button btnLogin, btnLoginVisitor;
-
+    private Button btnLogin, btnLoginGuest;
     private FirebaseAuth mAuth;
 
     @Override
@@ -41,56 +36,72 @@ public class SignInActivity extends AppCompatActivity {
         tvSignUp = findViewById(R.id.tv_signup);
         tvSignUp.getPaint().setUnderlineText(true);
         btnLogin = findViewById(R.id.btn_login);
-        btnLoginVisitor = findViewById(R.id.btn_login_visitor);
-
+        btnLoginGuest = findViewById(R.id.btn_login_guest);
         mAuth = FirebaseAuth.getInstance();
 
         View.OnClickListener btnListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (v.getId() == R.id.btn_login) {
-                    String user = etEmail.getText().toString();
+                    String email = etEmail.getText().toString();
                     String password = etPassword.getText().toString();
-                    if (user.isEmpty()) {
+                    if (email.isEmpty()) {
                         etEmail.setError("請輸入電子郵件地址");
                     }
                     if (password.isEmpty()) {
                         etPassword.setError("請輸入密碼");
                     }
                     if (etEmail.getError() == null && etPassword.getError() == null) {
-                        signIn(user, password);
-                        // TODO: Setting user permissions
+                        signIn(email, password);
                     }
-                } else if (v.getId() == R.id.btn_login_visitor) {
-                    // TODO: Setting visitor permissions
-                    Intent intent = new Intent();
-                    intent.setClass(SignInActivity.this, MainActivity.class);
-                    SignInActivity.this.startActivity(intent);
+                } else if (v.getId() == R.id.btn_login_guest) {
+                    sinInGuest();
                 } else if (v.getId() == R.id.tv_signup) {
                     Intent intent = new Intent();
                     intent.setClass(SignInActivity.this, SignUpActivity.class);
+                    SignInActivity.this.startActivity(intent);
+                } else if (v.getId() == R.id.tv_forget_password) {
+                    Intent intent = new Intent();
+                    intent.setClass(SignInActivity.this, ForgetPasswordActivity.class);
                     SignInActivity.this.startActivity(intent);
                 }
             }
         };
         btnLogin.setOnClickListener(btnListener);
-        btnLoginVisitor.setOnClickListener(btnListener);
+        btnLoginGuest.setOnClickListener(btnListener);
         tvSignUp.setOnClickListener(btnListener);
+        tvForgetPassword.setOnClickListener(btnListener);
     }
 
-    private void signIn(String user, String password) {
-        mAuth.signInWithEmailAndPassword(user, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
                     // 登入成功
-                    FirebaseUser user = mAuth.getCurrentUser();
                     Intent intent = new Intent();
                     intent.setClass(SignInActivity.this, MainActivity.class);
                     SignInActivity.this.startActivity(intent);
                 } else {
                     // 清空密碼欄
                     etPassword.setText("");
+                    // 登入失敗
+                    Toast.makeText(SignInActivity.this, "登入失敗", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void sinInGuest() {
+        mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // 登入成功
+                    Intent intent = new Intent();
+                    intent.setClass(SignInActivity.this, MainActivity.class);
+                    SignInActivity.this.startActivity(intent);
+                } else {
                     // 登入失敗
                     Toast.makeText(SignInActivity.this, "登入失敗", Toast.LENGTH_SHORT).show();
                 }
